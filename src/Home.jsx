dd6668d172740d6500b890ee1a78601fee9f208d
atrgouti <bilalatrgouti2002@gmail.com>
2024-01-21 01:38:34 +0100

    working on the anime collections

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,22 @@ import Announce from "./generalComponents/Announce";
 import Navbar from "./generalComponents/Navbar";
 import AnimeCategories from "./homeComponents/AnimeCategories";
 import cover from "/cove.jpg";
+import { useEffect, useState } from "react";
 function Home() {
+  const [windowWidth, setWindowWidth] = useState(window.outerWidth);
+
+  const handleResize = () => {
+    setWindowWidth(window.outerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className={style.home}>
       <Announce></Announce>
@@ -11,7 +26,7 @@ function Home() {
       <img
         src={cover}
         style={{
-          width: window.outerWidth > 450 ? "100%" : "200%",
+          width: windowWidth > 50 ? "100%" : "200%",
           margin: "0px",
           padding: "0px",
         }}
--- src/homeComponents/AnimeCategories.jsx
+++ src/homeComponents/AnimeCategories.jsx
@@ -1,14 +1,33 @@
+import styles from "./animeCategories.module.css";
+import onePiece from "/One_Piece.jpg";
+import naruto from "/naruto.jpg";
+import hantur from "/hunter.jpg";
+import attackOnTitans from "/attackOnTitans.jpg";
+
 function AnimeCategories() {
+  // const animecollections = [{ bilal: "bilal" }];
   return (
-    <div
-      style={{
-        height: "100vh",
-        width: "100%",
-        backgroundColor: "black",
-        marginTop: "-10px",
-        padding: "0px",
-      }}
-    ></div>
+    <div className={styles.animeCategories}>
+      <h1>Anime Collections</h1>
+      <div className={styles.collections}>
+        <div className={styles.collection}>
+          <img src={onePiece} style={{ width: "100%" }} alt="" />
+          <h3 style={{ textAlign: "center" }}>One piece</h3>
+        </div>
+        <div className={styles.collection}>
+          <img src={attackOnTitans} style={{ width: "100%" }} alt="" />
+          <h3 style={{ textAlign: "center" }}>Attack On Titan</h3>
+        </div>
+        <div className={styles.collection}>
+          <img src={naruto} style={{ width: "100%" }} alt="" />
+          <h3 style={{ textAlign: "center" }}>Naruto</h3>
+        </div>
+        <div className={styles.collection}>
+          <img src={hantur} style={{ width: "100%" }} alt="" />
+          <h3 style={{ textAlign: "center" }}>Hunter X Hunter</h3>
+        </div>
+      </div>
+    </div>
   );
 }
 
